d5239c60d80fe07b3aea621d5b8f6940471f8096
annumishra123 <anurag786332@gmail.com>
2020-02-27 00:28:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {ReactTinyLink} from 'react-tiny-link';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  render() {
+    return(
+      < div className="App">
+      <h3>Data... ...</h3>
+      <ReactTinyLink 
+        cardSize="small"
+        showGraphic={true}
+        maxLine={10}
+        minLine={1}
+        url="https://medium.com/better-programming/creating-a-simple-app-with-react-js-f6aa88998952"
+      />
+      <h3>Data... ...</h3>
+      <ReactTinyLink
+        cardSize="large"
+        showGraphic={true}
+        maxLine={5}
+        minLine={1}
+        url="https://www.hindustantimes.com/delhi-news/17-dead-in-delhi-violence-as-amit-shah-ajit-doval-chair-emergency-meetings/story-rqtBfiDfJGTezaFLLyZs6M.html"
+      />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
+
+
